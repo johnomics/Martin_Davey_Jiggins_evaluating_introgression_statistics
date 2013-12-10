3b6ca5acbacd2a51a5edd76c13d89548b4a82d9e
John Davey <johnomics@gmail.com>
2013-12-10 09:35:29 +0000

    Add plot files, model summary data and Heliconius genome data

---
+++ Figure_S1_S2.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+library(optparse)
+suppressMessages(library(ggplot2))
+library(plyr)
+
+options<-list(
+    make_option(c("-i","--input"),default=NULL,type="character",help="Input file")
+)
+
+opt<-parse_args(OptionParser(option_list=options))
+if (is.null(opt$input)) stop("Please supply an input file with -i")
+
+make.grid.plot<-function(models,stat) {
+    modelset<-subset(models,Stat==stat)
+    modelset$Alternate_t123<-factor(modelset$Alternate_t123,levels=seq(2,0.2,-0.2))
+
+    modelset$ModelType<-revalue(modelset$ModelType,c("Ancestral structure"="Ancestral Structure","gene flow P2-P3"="Gene Flow P2-P3","gene flow P3-P2"="Gene Flow P3-P2"))
+    modelset$Stat<-revalue(modelset$Stat,c(mfD0="f"))
+
+    names(modelset)[1]<-"Bgt123"
+    names(modelset)[3]<-"Alt123"
+    ggplot(modelset,
+    	   aes(Background_t21,Alternate_t23,size=OutlierAlternatePC,colour=ModelType)
+    	  )+
+    	   geom_point()+
+    	   facet_grid(Alt123~Bgt123,labeller=label_both)+
+    	   scale_size(paste("% predicted by",unique(modelset$Stat)),limits=c(0,100),breaks=seq(0,100,20),range=c(1,9))+
+    	   scale_x_continuous(expression(paste(Bgt[21])),breaks=seq(0,2,0.2))+
+    	   scale_y_continuous(expression(paste(Alt[23])),breaks=seq(0,2,0.2))+
+    	   theme_bw(base_size=10)
+}
+
+
+models<-read.delim(opt$input)
+
+pdf("Figure_S1.pdf",21,21)
+make.grid.plot(models,"D")
+dev.off()
+
+pdf("Figure_S2.pdf",21,21)
+make.grid.plot(models,"mfD0")
+dev.off()
+
+warninglist<-warnings()
+if (!(is.null(warninglist))) warninglist
---
+++ Figure_S3_S4.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+suppressMessages(library(ggplot2))
+suppressMessages(library(reshape))
+library(plyr)
+library(stringr)
+
+options(width=200)
+load.with.window.size<-function(filename) {
+    t<-read.delim(filename)
+    t$Size<-as.numeric(str_match(filename,"size([[:digit:]]+)")[1,2])
+    t[c("Background_t123","Background_t21","Alternate_t123","Alternate_t23","File","Stat","ModelType","Size","OutlierAlternatePC")]
+}
+
+models<-rbind(load.with.window.size("model_files_win10000_size5000.partition.summary.tsv"),
+              load.with.window.size("model_files_win5000_size10000.partition.summary.tsv"),
+              load.with.window.size("model_files_win2500_size20000.partition.summary.tsv")
+              )
+
+kbwin<-function(x) {
+    xkb<-as.numeric(x)/1000
+    out<-paste(xkb,"kb windows")
+    out
+}
+
+models$Stat<-revalue(models$Stat,c("mfD0"="f"))
+
+models$Size<-factor(models$Size,levels=sort(unique(models$Size)))
+models$Size<-mapvalues(models$Size,from=levels(models$Size),to=kbwin(levels(models$Size)))
+
+
+
+models.f<-rename(subset(models,Stat=="f"),c("OutlierAlternatePC"="f_Accuracy"))
+models.D<-rename(subset(models,Stat=="D"),c("OutlierAlternatePC"="D_Accuracy"))
+
+models.figS3<-merge(models.f[c("File","ModelType","Size","f_Accuracy")],models.D[c("File","D_Accuracy")],by="File")
+
+pdf("Figure_S3.pdf",10.5,3.5)
+
+ggplot(models.figS3,aes(f_Accuracy,D_Accuracy,colour=ModelType))+
+       facet_grid(.~Size)+
+       geom_point()+
+       geom_abline(intercept=0,slope=1,linetype="dashed")+
+       xlim(c(0,100))+
+       ylim(c(0,100))+
+       xlab("% predicted by f")+
+       ylab("% predicted by D")+
+       coord_fixed()+
+       theme_bw()
+dev.off()
+
+models.5kb<-rename(subset(models,Size=="5 kb windows"),c("OutlierAlternatePC"="Accuracy_5kb"))
+models.10_20kb<-rename(subset(models,Size != "5 kb windows"),c("OutlierAlternatePC"="Accuracy_others"))
+
+models.figS4<-merge(models.5kb[c("Background_t123","Background_t21","Alternate_t123","Alternate_t23","Stat","ModelType","Accuracy_5kb")],
+                    models.10_20kb[c("Background_t123","Background_t21","Alternate_t123","Alternate_t23","Stat","Size","Accuracy_others")],
+                    by=c("Background_t123","Background_t21","Alternate_t123","Alternate_t23","Stat"))
+
+pdf("Figure_S4.pdf",8,7)
+ggplot(models.figS4,aes(Accuracy_5kb,Accuracy_others,colour=ModelType))+
+       facet_grid(Size~Stat)+
+       geom_point()+
+       geom_abline(intercept=0,slope=1,linetype="dashed")+
+       xlim(c(0,100))+
+       ylim(c(0,100))+
+       xlab("Accuracy (5 kb windows)")+
+       ylab("Accuracy")+
+       coord_fixed()+
+       theme_bw()
+dev.off()
